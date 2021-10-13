c9583ae16abd439aa5a73ae625fb5ee29fd1434c
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-13 17:23:06 -0300

    Route names

---
+++ src/constants/routeNames.ts
@@ -0,0 +1,16 @@
+export const AUTH_STACK = {
+  AUTH: 'AUTH',
+  REGISTER: 'REGISTER',
+  FORGOT_PASSWORD: 'FORGOT_PASSWORD',
+  LOGIN: 'LOGIN',
+};
+
+export const MAIN_STACK = {
+  MAIN: 'MAIN',
+  HOME: 'HOME',
+};
+
+export default {
+  MAIN_STACK,
+  AUTH_STACK,
+};
