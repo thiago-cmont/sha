9cdcff57f36e3055ff52b35c3e3fcb9935d166af
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-15 15:30:26 -0300

    Contact pages

---
+++ src/pages/Contact/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Contact: React.FC = () => {
+  return (
+    <S.Container>
+      <S.PageTitle>Contact</S.PageTitle>
+    </S.Container>
+  );
+};
+
+export default Contact;
