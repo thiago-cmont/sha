06963aa311ab91168e9ece2564ec2ae2047ac39d
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-15 15:30:47 -0300

    Forgot password

--- src/pages/forgotPassword/index.tsx
+++ src/pages/forgotPassword/index.tsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
+import {useNavigation} from '@react-navigation/native';
+import {Formik} from 'formik';
+import * as yup from 'yup';
+
+import {appIcon} from '../../assets';
+import {AUTH_STACK} from '../../constants/routeNames';
 import * as S from './styles';
 
-const ForgotPassword: React.FC = () => (
-  <S.Container>
-    <S.PageTitle>Forgot Password</S.PageTitle>
-  </S.Container>
-);
+const ForgotPassword: React.FC = () => {
+  const yupValidationSchema = yup.object().shape({
+    email: yup.string().required().label('Email'),
+  });
+  const emailInputRef = useRef();
+  const navigation = useNavigation();
+
+  const handleSubmitForm = values => {
+    navigation.navigate(AUTH_STACK.LOGIN);
+  };
+  return (
+    <S.Container>
+      <S.AppIcon source={appIcon} />
+      <S.AvoidKeyboard>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          onSubmit={handleSubmitForm}
+          validationSchema={yupValidationSchema}
+          validateOnChange={false}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            setFieldError,
+          }) => (
+            <>
+              <S.InputWrapper
+                ref={emailInputRef}
+                hasHelperText
+                placeholder="Email"
+                value={values.email}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                autoCapitalize="words"
+                error={errors.email}
+              />
+              <S.LoginButton
+                title="Recover Password"
+                variant="outline"
+                onPress={e => {
+                  if (values.email === '') {
+                    setFieldError('email', 'Enter a valid email.');
+                  } else {
+                    handleSubmit(e);
+                  }
+                }}
+              />
+            </>
+          )}
+        </Formik>
+      </S.AvoidKeyboard>
+    </S.Container>
+  );
+};
 
 export default ForgotPassword;
--- src/pages/forgotPassword/styles.ts
+++ src/pages/forgotPassword/styles.ts
@@ -1,11 +1,49 @@
+import {Platform} from 'react-native';
+
+import {vs, s} from 'react-native-size-matters';
 import styled from 'styled-components/native';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import {Subtitle} from '../../components/Typography';
+import {HEADER_HEIGHT} from '../../utils/dimensions';
+
+const isIOS = Platform.OS === 'ios';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: center;
+  padding: 20px;
+  background-color: ${({theme}) => theme.colors.primary};
 `;
-export const PageTitle = styled.Text`
-  font-weight: bold;
-  font-size: 25px;
+
+export const AppIcon = styled.Image.attrs(({theme}) => ({
+  tintColor: theme.colors.iconLight,
+  resizeMode: 'contain',
+}))`
+  width: ${vs(150)}px;
+  height: ${vs(150)}px;
+`;
+
+export const AvoidKeyboard = styled.KeyboardAvoidingView.attrs({
+  behavior: isIOS ? 'padding' : 'height',
+  keyboardVerticalOffset: HEADER_HEIGHT,
+})`
+  padding-top: ${vs(50)}px;
+  flex: 1;
+`;
+
+export const InputWrapper = styled(Input)`
+  width: ${s(320)}px;
+`;
+
+export const LoginButton = styled(Button)``;
+
+export const ForgotPassword = styled(Subtitle)`
+  align-self: center;
+  margin-top: 10px;
+`;
+
+export const Registar = styled(Subtitle)`
+  align-self: center;
 `;
