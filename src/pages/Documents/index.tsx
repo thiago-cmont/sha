34fc18b90597bcb58da68109cb668144dcb1ebb5
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-15 15:30:33 -0300

    Document pages

---
+++ src/pages/Documents/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Documents: React.FC = () => {
+  return (
+    <S.Container>
+      <S.PageTitle>Docs</S.PageTitle>
+    </S.Container>
+  );
+};
+
+export default Documents;
