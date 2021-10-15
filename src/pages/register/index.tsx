8d3be8f01deb9480a27e86688e8af99c816a784c
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-15 15:31:22 -0300

    register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,11 +1,161 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
+import {useNavigation} from '@react-navigation/native';
+import {Formik} from 'formik';
+import * as yup from 'yup';
+
+import {appIcon} from '../../assets';
+import {MAIN_STACK} from '../../constants/routeNames';
 import * as S from './styles';
 
-const Register: React.FC = () => (
-  <S.Container>
-    <S.PageTitle>Register</S.PageTitle>
-  </S.Container>
-);
+const Register: React.FC = () => {
+  const yupValidationSchema = yup.object().shape({
+    name: yup.string().required().label('Name'),
+    apartment: yup.string().required().label('Apartment'),
+    email: yup.string().required().label('Email'),
+    password: yup.string().required().label('Password'),
+    apartmentComplexName: yup.string().required().label('ApartmentComplexName'),
+    level: yup.string().required().label('Level'),
+  });
+
+  const level = [
+    {
+      id: 1,
+      label: 'Apartment Owner',
+    },
+    {
+      id: 2,
+      label: 'Apartment Manager',
+    },
+  ];
+  const nameInputRef = useRef();
+  const apartmentInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const aprtmentComplexNameInputRef = useRef();
+  const levelInputRef = useRef();
+  const navigation = useNavigation();
+
+  const handleSubmitForm = values => {
+    navigation.navigate(MAIN_STACK.MAIN, {screen: MAIN_STACK.HOME});
+  };
+  return (
+    <S.Container>
+      <S.RegisterScrollView showsVerticalScrollIndicator={false}>
+        <S.AppIcon source={appIcon} />
+        <S.AvoidKeyboard>
+          <Formik
+            initialValues={{
+              name: '',
+              apartment: '',
+              email: '',
+              password: '',
+              apartmentComplexName: '',
+              level: '',
+            }}
+            onSubmit={handleSubmitForm}
+            validationSchema={yupValidationSchema}
+            validateOnChange={false}>
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              setFieldError,
+              setFieldValue,
+            }) => (
+              <>
+                <S.InputWrapper
+                  ref={nameInputRef}
+                  hasHelperText
+                  placeholder="Name"
+                  value={values.name}
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  autoCapitalize="words"
+                  error={errors.name}
+                />
+                <S.InputWrapper
+                  ref={emailInputRef}
+                  hasHelperText
+                  placeholder="Email"
+                  value={values.email}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('password')}
+                  autoCapitalize="words"
+                  error={errors.email}
+                />
+                <S.InputWrapper
+                  ref={passwordInputRef}
+                  hasHelperText
+                  placeholder="Password"
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  autoCapitalize="words"
+                  error={errors.password}
+                />
+                <S.InputWrapper
+                  ref={apartmentInputRef}
+                  hasHelperText
+                  placeholder="Apartment"
+                  value={values.apartment}
+                  onChangeText={handleChange('apartment')}
+                  onBlur={handleBlur('apartment')}
+                  autoCapitalize="words"
+                  error={errors.apartment}
+                />
+                <S.InputWrapper
+                  ref={aprtmentComplexNameInputRef}
+                  hasHelperText
+                  placeholder="Apartment Complex Name"
+                  value={values.apartmentComplexName}
+                  onChangeText={handleChange('apartmentComplexName')}
+                  onBlur={handleBlur('apartmentComplexName')}
+                  autoCapitalize="words"
+                  error={errors.apartmentComplexName}
+                />
+                <S.SelectWrapper
+                  ref={levelInputRef}
+                  data={level}
+                  error={errors.level}
+                  defaultText="Level"
+                  onChange={selected => {
+                    setFieldValue('level', selected?.label);
+                  }}
+                />
+                <S.LoginButton
+                  title="Create Account"
+                  variant="outline"
+                  onPress={e => {
+                    if (values.name === '') {
+                      setFieldError('name', 'Enter a valid name');
+                    } else if (values.email === '') {
+                      setFieldError('email', 'Enter a valid email.');
+                    } else if (values.password === '') {
+                      setFieldError('password', 'Enter a valid password');
+                    } else if (values.name === '') {
+                      setFieldError('name', 'Enter a valid name');
+                    } else if (values.apartment === '') {
+                      setFieldError('apartment', 'Enter a valid Apartment');
+                    } else if (values.apartmentComplexName === '') {
+                      setFieldError(
+                        'apartmentComplexName',
+                        'Enter a valid apartment complex name',
+                      );
+                    } else {
+                      handleSubmit(e);
+                    }
+                  }}
+                />
+              </>
+            )}
+          </Formik>
+        </S.AvoidKeyboard>
+      </S.RegisterScrollView>
+    </S.Container>
+  );
+};
 
 export default Register;
--- src/pages/register/styles.ts
+++ src/pages/register/styles.ts
@@ -1,11 +1,59 @@
+import {Platform} from 'react-native';
+
+import {vs, s} from 'react-native-size-matters';
 import styled from 'styled-components/native';
+
+import Button from '../../components/Button';
+import FWInput from '../../components/Input';
+import FWSelect from '../../components/Select';
+import {Subtitle} from '../../components/Typography';
+import {HEADER_HEIGHT} from '../../utils/dimensions';
+
+const isIOS = Platform.OS === 'ios';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: center;
+  background-color: ${({theme}) => theme.colors.primary};
 `;
-export const PageTitle = styled.Text`
-  font-weight: bold;
-  font-size: 25px;
+
+export const AppIcon = styled.Image.attrs(({theme}) => ({
+  tintColor: theme.colors.iconLight,
+  resizeMode: 'contain',
+}))`
+  width: ${vs(150)}px;
+  height: ${vs(150)}px;
+`;
+
+export const AvoidKeyboard = styled.KeyboardAvoidingView.attrs({
+  keyboardVerticalOffset: HEADER_HEIGHT,
+})`
+  padding-top: ${vs(50)}px;
+  flex: 1;
+`;
+
+export const RegisterScrollView = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingBottom: vs(50),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})`
+  flex: 1;
+`;
+
+export const InputWrapper = styled(FWInput)`
+  width: ${s(320)}px;
+`;
+
+export const SelectWrapper = styled(FWSelect)``;
+export const LoginButton = styled(Button)``;
+
+export const ForgotPassword = styled(Subtitle)`
+  align-self: center;
+  margin-top: 10px;
+`;
+
+export const Registar = styled(Subtitle)`
+  align-self: center;
 `;
