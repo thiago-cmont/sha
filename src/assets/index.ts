e52d4c9f642e1ab616412bc7fa6058b16a32ae9c
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-15 15:26:10 -0300

    Assets export

---
+++ src/assets/index.ts
@@ -0,0 +1,25 @@
+import appIcon from './appIcon.png';
+import contact from './contact.png';
+import contactOutline from './contactOutline.png';
+import docs from './docs.png';
+import docsOutline from './docsOutline.png';
+import dollar from './dollar.png';
+import dollarOutline from './dollarOutline.png';
+import home from './home.png';
+import homeOutline from './homeOutline.png';
+import places from './places.png';
+import placesOutline from './placesOutline.png';
+
+export {
+  appIcon,
+  home,
+  homeOutline,
+  contact,
+  contactOutline,
+  dollar,
+  dollarOutline,
+  places,
+  placesOutline,
+  docs,
+  docsOutline,
+};
