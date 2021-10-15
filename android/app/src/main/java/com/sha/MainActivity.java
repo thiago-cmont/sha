e02c8b83568e9de44fbfdb07af5e092e90bca867
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-15 15:25:12 -0300

    React Native Splash Screen Setup

--- android/app/src/main/java/com/sha/MainActivity.java
+++ android/app/src/main/java/com/sha/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sha;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-
+import org.devio.rn.splashscreen.SplashScreen;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -10,6 +10,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
+     SplashScreen.show(this);
     return "sha";
   }
 }
