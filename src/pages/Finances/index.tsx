58fe8a9b7124d0b63a235acf3bd4a670bd77b935
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2021-10-15 15:30:40 -0300

    Finances pages

---
+++ src/pages/Finances/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Finances: React.FC = () => {
+  return (
+    <S.Container>
+      <S.PageTitle>Finances</S.PageTitle>
+    </S.Container>
+  );
+};
+
+export default Finances;
